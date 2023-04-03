a4027beec8fe5436a686d80d197e3eddff506e50
diego40g <diegopaznaula@hotmail.com>
2023-04-02 22:46:09 -0500

    default example to manage object

---
+++ Curso_LaWebEmpiezaAqui/002_variables/objects.ts
@@ -0,0 +1,11 @@
+var person = {
+    firstName: "Diego",
+    lastName: "Paz",
+    age: 16,
+    weight: 120,
+}
+
+console.log(person.firstName)
+console.log(person.lastName)
+console.log(person.age)
+console.log(person.weight)
