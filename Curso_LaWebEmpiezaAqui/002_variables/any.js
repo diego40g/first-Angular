84f5deaaffbd52a2de5507f68c9491c5d6a77482
diego40g <diegopaznaula@hotmail.com>
2023-04-02 23:06:56 -0500

    default example to understand the use of type any

---
+++ Curso_LaWebEmpiezaAqui/002_variables/any.js
@@ -0,0 +1,10 @@
+var songTitle = "15 canciones";
+//songTitle = 115
+// using of external API services
+// because can change the type of var
+var anyIdeaRequest = "Music";
+console.log(anyIdeaRequest);
+anyIdeaRequest = 155;
+console.log(anyIdeaRequest);
+anyIdeaRequest = true;
+console.log(anyIdeaRequest);
---
+++ Curso_LaWebEmpiezaAqui/002_variables/any.ts
@@ -0,0 +1,11 @@
+var songTitle:string = "15 canciones";
+//songTitle = 115
+
+// using of external API services
+// because can change the type of var
+var anyIdeaRequest:any = "Music";
+console.log(anyIdeaRequest)
+anyIdeaRequest = 155
+console.log(anyIdeaRequest)
+anyIdeaRequest = true
+console.log(anyIdeaRequest)
