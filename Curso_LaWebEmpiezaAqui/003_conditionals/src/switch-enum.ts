558e6dc069c3a020897de11a71c3c3f823ecc10f
diego40g <diegopaznaula@hotmail.com>
2023-04-04 23:48:35 -0500

    understand switch case, enum and comments

---
+++ Curso_LaWebEmpiezaAqui/003_conditionals/src/switch-enum.ts
@@ -0,0 +1,62 @@
+var sendNumbers: number = 51;
+
+switch (sendNumbers) {
+    case 25:
+        console.log("Son veinte y cinco")
+        break;
+    case 30: 
+        console.log("Son treinta")
+        break
+    case 50: 
+        console.log("Son cincuenta")
+        break
+    default:
+        console.log("No cumplio ninguna condición")
+        break;
+}
+
+//1 Songs menu
+//2 Albums menu
+//3 Profile menu
+
+var menus: number = 2;
+
+/*switch (menus){
+    case 1:
+        console.log("Acceso a las canciones")
+        break;
+    case 2:
+        console.log("Acceso a los albumes")
+        break;
+    case 3:
+        console.log("Acceso al perfil")
+        break;
+    default:
+        console.log("El menu aun no se a implementado")
+}*/
+
+//ENUMS
+/*enum tipeMenu{
+    songs = 1,
+    albums = 2,
+    profile = 3
+}*/
+enum tipeMenu{
+    songs = 1,
+    albums,
+    profile
+}
+
+switch (menus){
+    case tipeMenu.songs:
+        console.log("Acceso a las canciones")
+        break;
+    case tipeMenu.albums:
+        console.log("Acceso a los albumes")
+        break;
+    case tipeMenu.profile:
+        console.log("Acceso al perfil")
+        break;
+    default:
+        console.log("El menu aun no se a implementado")
+}
