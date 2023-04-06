65d592110f2c67b037de22588bb1a42370cf2228
diego40g <diegopaznaula@hotmail.com>
2023-04-05 22:43:34 -0500

    understand cicle for with number and array example

---
+++ Curso_LaWebEmpiezaAqui/004_cicles/src/forCicles.js
@@ -0,0 +1,8 @@
+for (var counter = 0; counter <= 5; counter++) {
+    console.log("".concat(counter, "/6"));
+}
+var firstNameList = ["Ana", "Diego", "Ismael", "Richard"];
+console.log("Valor length del arreglo = ", firstNameList.length);
+for (var counterName = 0; counterName < firstNameList.length; counterName++) {
+    console.log("".concat(firstNameList[counterName]));
+}
---
+++ Curso_LaWebEmpiezaAqui/004_cicles/src/forCicles.ts
@@ -0,0 +1,9 @@
+for(var counter: number = 0; counter <= 5; counter++){
+    console.log(`${counter}/6`)
+}
+
+var firstNameList: Array<string> = ["Ana","Diego","Ismael","Richard"]
+console.log("Valor length del arreglo = ", firstNameList.length)
+for (var counterName: number = 0; counterName < firstNameList.length; counterName++ ) {
+    console.log(`${firstNameList[counterName]}`)
+}
