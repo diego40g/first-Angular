53a8a94e3afd1ee9fabf3fbd8c7bd9d7d4abd6c8
diego40g <diegopaznaula@hotmail.com>
2023-04-06 23:03:20 -0500

    understand simple functions greeting and adding function

---
+++ Curso_LaWebEmpiezaAqui/005_functions/src/simpleFunctions.js
@@ -0,0 +1,14 @@
+function printGreeting() {
+    console.log("Hi");
+    console.log("Hello");
+}
+printGreeting();
+function addFun() {
+    var num1 = 5;
+    var num2 = 5;
+    var total = num1 + num2;
+    console.log("El Total es = ", total);
+}
+addFun();
+addFun();
+addFun();
---
+++ Curso_LaWebEmpiezaAqui/005_functions/src/simpleFunctions.ts
@@ -0,0 +1,18 @@
+function printGreeting(){
+    console.log("Hi");
+    console.log("Hello");
+}
+
+printGreeting();
+
+function addFun (){
+    var num1: number = 5;
+    var num2: number = 5;
+    var total: number = num1+ num2;
+    
+    console.log("El Total es = ",total);
+}
+
+addFun();
+addFun();
+addFun();
