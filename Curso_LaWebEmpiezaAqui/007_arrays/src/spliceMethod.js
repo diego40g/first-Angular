5ad0fbcd544ec01c87b2cfb29b53bf5b5f83b531
diego40g <diegopaznaula@hotmail.com>
2023-04-07 22:27:30 -0500

    understand the splice method into the arrays

---
+++ Curso_LaWebEmpiezaAqui/007_arrays/src/spliceMethod.js
@@ -0,0 +1,6 @@
+var students = ["Ana", "Diego", "Ismale", "Richard"];
+console.log(students);
+var positionDelete = 1;
+var totalDelete = 1;
+students.splice(positionDelete, totalDelete);
+console.log(students);
---
+++ Curso_LaWebEmpiezaAqui/007_arrays/src/spliceMethod.ts
@@ -0,0 +1,7 @@
+var students: string[] = ["Ana", "Diego", "Ismale", "Richard"]
+console.log(students)
+
+const positionDelete:number = 1;
+const totalDelete:number = 1
+students.splice(positionDelete, totalDelete)
+console.log(students)
