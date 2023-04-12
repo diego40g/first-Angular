18c14450178e000923655a9cbc720206a755a60c
diego40g <diegopaznaula@hotmail.com>
2023-04-11 23:31:27 -0500

    create default component

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/001_firstProject/firstProject/src/app/titles/titles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titles',
+  templateUrl: './titles.component.html',
+  styleUrls: ['./titles.component.scss']
+})
+export class TitlesComponent {
+
+}
