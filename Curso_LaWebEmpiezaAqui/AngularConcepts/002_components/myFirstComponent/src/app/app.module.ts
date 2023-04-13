64065c2f47b722282227b2a76bec013c3102b2b9
diego40g <diegopaznaula@hotmail.com>
2023-04-12 23:47:36 -0500

    create a defautl component titles

--- Curso_LaWebEmpiezaAqui/AngularConcepts/002_components/myFirstComponent/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/002_components/myFirstComponent/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitlesComponent } from './titles/titles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlesComponent
   ],
   imports: [
     BrowserModule
