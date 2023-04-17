679c46d04cba6ef9e59e8d431afcab02dd1f6ec7
diego40g <diegopaznaula@hotmail.com>
2023-04-16 23:07:42 -0500

    create new variables and doing interpolation with objects or strings

--- Curso_LaWebEmpiezaAqui/AngularConcepts/002_components/myFirstComponent/src/app/titles/titles.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/002_components/myFirstComponent/src/app/titles/titles.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./titles.component.css']
 })
 export class TitlesComponent implements OnInit{
+  firstName: string = "Diego";
+
+  student: any = {
+    firstName: "Ismael",
+    lastName: "Paz",
+    age: 22
+  }
+
   constructor() {}
 
   ngOnInit() {
