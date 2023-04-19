a8237c4d59e3e72825de27975ffc816d21994f10
diego40g <diegopaznaula@hotmail.com>
2023-04-18 22:48:13 -0500

    adding default new component input

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/003_events/src/app/event-input/event-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-input',
+  templateUrl: './event-input.component.html',
+  styleUrls: ['./event-input.component.css']
+})
+export class EventInputComponent {
+
+}
