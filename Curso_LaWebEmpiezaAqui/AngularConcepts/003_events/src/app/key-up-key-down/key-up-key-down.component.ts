e2453859b9e204b6fcef75653bac178bf7f8fc90
diego40g <diegopaznaula@hotmail.com>
2023-04-20 22:06:45 -0500

    understand the using of key down and up

--- Curso_LaWebEmpiezaAqui/AngularConcepts/003_events/src/app/key-up-key-down/key-up-key-down.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/003_events/src/app/key-up-key-down/key-up-key-down.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./key-up-key-down.component.css']
 })
 export class KeyUpKeyDownComponent {
-
+  keyUp(event: any){
+    if(event.key == "Enter"){
+      console.log("keyUp - Soltar tecla")
+    }
+  }
+  keyDown(event: any){
+    if(event.key == "Enter"){
+      console.log("keyDown - Precionar tecla")
+    }
+  }
 }
