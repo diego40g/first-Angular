02907484fa35579ddd9e22701dd68df8845eeff8
diego40g <diegopaznaula@hotmail.com>
2023-04-21 22:30:49 -0500

    adding new component ngif to manage directive if

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgIfComponent
   ],
   imports: [
     BrowserModule
