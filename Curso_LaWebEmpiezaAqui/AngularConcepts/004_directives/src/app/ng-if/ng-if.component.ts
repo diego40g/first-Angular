e21a09696f104a5de59e6f9303b8fedcf99a5b9c
diego40g <diegopaznaula@hotmail.com>
2023-04-21 22:43:06 -0500

    understand the directory ng if

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-if/ng-if.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-if/ng-if.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ng-if',
   templateUrl: './ng-if.component.html',
   styleUrls: ['./ng-if.component.css']
 })
-export class NgIfComponent {
+export class NgIfComponent implements OnInit{
+  loading: boolean = true;
 
+  ngOnInit(){
+    setTimeout(() => {
+      this.loading=false
+    }, 3000)
+  }
+
+  reload(){
+    this.loading=!this.loading;
+  }
 }
