9c6fca08827c592dfc5e3b6b8bf99db2de6cccaf
diego40g <diegopaznaula@hotmail.com>
2023-04-23 00:05:27 -0500

    default example to understand a ng switch

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-switch/ng-switch.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-switch/ng-switch.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./ng-switch.component.css']
 })
 export class NgSwitchComponent {
+  tab: string = '';
 
+  changeTab(tab: string){
+    this.tab=tab;
+  }
 }
