640c8b3c4dd9690dd3a964e7df531207d98499b3
diego40g <diegopaznaula@hotmail.com>
2023-04-22 23:50:45 -0500

    adding new declaration for new component ngFor

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
 import { NgForComponent } from './ng-for/ng-for.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgIfComponent,
-    NgForComponent
+    NgForComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
