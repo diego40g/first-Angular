78daa2fcf115dc49438b587cbda44121958d24d8
diego40g <diegopaznaula@hotmail.com>
2023-04-22 23:49:48 -0500

    basic example to understand ng for

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-for/ng-for.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-for/ng-for.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+interface Product{
+  name: string;
+  stock: number;
+  creator: string;
+  dueDate: Date;
+}
+
 @Component({
   selector: 'app-ng-for',
   templateUrl: './ng-for.component.html',
   styleUrls: ['./ng-for.component.css']
 })
 export class NgForComponent {
+  firstNames: Array<string> = ["Ana","Diego","Ismael","Richard"]
 
+  products: Array<Product> = [
+    {
+      name: "Rice",
+      stock: 15,
+      creator: "Industrial Distribution",
+      dueDate: new Date('04/25/2023')
+    },
+    {
+      name: "Soda",
+      stock: 200,
+      creator: "coca Cola",
+      dueDate: new Date('01/15/2024')
+    }
+  ]
 }
