c3b4835322bde827c706994d87eb106073b52aaa
diego40g <diegopaznaula@hotmail.com>
2023-04-23 23:46:17 -0500

    a basic example to understand manage class in angular

--- Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-class/ng-class.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/004_directives/src/app/ng-class/ng-class.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Product{
+  name: string;
+  stock: number;
+  creator: string;
+  dueDate: Date;
+  isImportant: boolean;
+  isBigText: boolean;
+}
+
 @Component({
   selector: 'app-ng-class',
   templateUrl: './ng-class.component.html',
@@ -7,4 +16,27 @@ import { Component } from '@angular/core';
 })
 export class NgClassComponent {
 
+  products: Array<Product> = [
+    {
+      name: "Rice",
+      stock: 15,
+      creator: "Industrial Distribution",
+      dueDate: new Date('04/25/2023'),
+      isImportant: true,
+      isBigText: true
+    },
+    {
+      name: "Rice",
+      stock: 15,
+      creator: "Industrial Distribution",
+      dueDate: new Date('04/25/2023'),
+      isImportant: false,
+      isBigText: true
+    }
+  ]
+
+  showSquart: boolean = false;
+  changeBackground(){
+    this.showSquart = !this.showSquart;
+  }
 }
