4cd06949ab2624c5bce4f09ab2c92fcd5a3ad579
diego40g <diegopaznaula@hotmail.com>
2023-04-24 22:02:42 -0500

    adding declaration of new component boostrap example

--- Curso_LaWebEmpiezaAqui/AngularConcepts/005_boostrap/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/005_boostrap/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoostrapExampleComponent } from './boostrap-example/boostrap-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoostrapExampleComponent
   ],
   imports: [
     BrowserModule
