ffd95ea450f93eabe32119cabd7dd5bf4d6a175e
diego40g <diegopaznaula@hotmail.com>
2023-04-25 23:01:15 -0500

    understand basic example to boostrap form

--- Curso_LaWebEmpiezaAqui/AngularConcepts/005_boostrap/src/app/boostrap-form/boostrap-form.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/005_boostrap/src/app/boostrap-form/boostrap-form.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+class Student{
+  public firstName: string;
+  public lastName: string;
+
+  constructor(){
+    this.firstName='';
+    this.lastName='';
+  }
+
+
+}
+
 @Component({
   selector: 'app-boostrap-form',
   templateUrl: './boostrap-form.component.html',
   styleUrls: ['./boostrap-form.component.css']
 })
 export class BoostrapFormComponent {
+  alumno: Student = new Student();
+  studentsList: Array<Student> = new Array<Student>();
 
+  addingStudent(){
+    this.studentsList.push({
+      firstName: this.alumno.firstName,
+      lastName: this.alumno.lastName
+    })
+    this.alumno.firstName='';
+    this.alumno.lastName='';
+
+    console.log(this.studentsList);
+  }
 }
