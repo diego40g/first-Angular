946b50a8b258efb3fee1cfc27263e8b08017a0b8
diego40g <diegopaznaula@hotmail.com>
2023-04-26 22:18:14 -0500

    declaration the new component upper case

--- Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UpperCaseTextComponent } from './upper-case-text/upper-case-text.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperCaseTextComponent
   ],
   imports: [
     BrowserModule
