a4f240d67f68361ac24721d94bde8285d25716c1
diego40g <diegopaznaula@hotmail.com>
2023-04-26 22:24:09 -0500

    declaration the new component lower case

--- Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UpperCaseTextComponent } from './upper-case-text/upper-case-text.component';
+import { LowerCaseTextComponent } from './lower-case-text/lower-case-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UpperCaseTextComponent
+    UpperCaseTextComponent,
+    LowerCaseTextComponent
   ],
   imports: [
     BrowserModule
