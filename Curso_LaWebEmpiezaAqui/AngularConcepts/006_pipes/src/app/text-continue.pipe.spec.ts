fcb6ebe4ced32e8afc16ca7b7b32c721fd44f609
diego40g <diegopaznaula@hotmail.com>
2023-04-29 22:27:12 -0500

    understant a basic example to create pipe conditional

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/text-continue.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TextContinuePipe } from './text-continue.pipe';
+
+describe('TextContinuePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TextContinuePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
