1cc78c210fe7a09a28aa99e8d47b841607754c44
diego40g <diegopaznaula@hotmail.com>
2023-04-29 22:27:31 -0500

    understand a basic example with using a our own pipe

--- Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/our-own-pipe/our-own-pipe.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/006_pipes/src/app/our-own-pipe/our-own-pipe.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./our-own-pipe.component.css']
 })
 export class OurOwnPipeComponent {
-
+  loremText: string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta aspernatur laudantium sequi eaque neque autem vero nulla fuga officiis aliquid, deserunt facilis ab aut quidem culpa at voluptatum dolorum quia?"
 }
