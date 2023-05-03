4ee7728c5ffe04e5af4d3d796c5999751cead038
diego40g <diegopaznaula@hotmail.com>
2023-05-02 21:41:43 -0500

    adding new component titles

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/007_routing/src/app/titles/titles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titles',
+  templateUrl: './titles.component.html',
+  styleUrls: ['./titles.component.css']
+})
+export class TitlesComponent {
+  title: string = "Hi it's a title who use title case"
+}
