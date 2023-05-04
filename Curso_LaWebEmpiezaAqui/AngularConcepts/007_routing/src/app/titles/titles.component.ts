22178c603618e982cef2ed88c88137dd2a0cef61
diego40g <diegopaznaula@hotmail.com>
2023-05-03 20:50:46 -0500

    change static text to user and password

--- Curso_LaWebEmpiezaAqui/AngularConcepts/007_routing/src/app/titles/titles.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/007_routing/src/app/titles/titles.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-titles',
   templateUrl: './titles.component.html',
-  styleUrls: ['./titles.component.css']
+  styleUrls: ['./titles.component.sass']
 })
 export class TitlesComponent {
-  title: string = "Hi it's a title who use title case"
+  email: string = ''
+  password: string = ''
+
+  login(){
+    alert('Ha precionado doble click')
+  }
+
+  maxSize(event: any){
+    event.srcElement.style.width='1000px'
+    event.srcElement.style.heigth='400px'
+    event.srcElement.style.border='1px solid orange'
+  }
+  minSize(event: any){
+    event.srcElement.style.width='500px'
+    event.srcElement.style.heigth='200px'
+    event.srcElement.style.border='1px solid green'
+  }
 }
