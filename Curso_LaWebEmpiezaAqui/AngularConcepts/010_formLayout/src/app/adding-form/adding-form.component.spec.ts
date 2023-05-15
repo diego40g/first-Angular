b57ec82a0abf7067ad7782a86e806a121a9dcdf2
diego40g <diegopaznaula@hotmail.com>
2023-05-14 22:29:25 -0500

    change default principal view to a new component

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/010_formLayout/src/app/adding-form/adding-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddingFormComponent } from './adding-form.component';
+
+describe('AddingFormComponent', () => {
+  let component: AddingFormComponent;
+  let fixture: ComponentFixture<AddingFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddingFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/010_formLayout/src/app/adding-form/adding-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-form',
+  templateUrl: './adding-form.component.html',
+  styleUrls: ['./adding-form.component.sass']
+})
+export class AddingFormComponent {
+
+}
