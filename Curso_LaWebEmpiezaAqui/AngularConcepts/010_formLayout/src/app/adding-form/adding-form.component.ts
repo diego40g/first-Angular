680e320f9ac451229d8450627d826eda9a1db6b3
diego40g <diegopaznaula@hotmail.com>
2023-05-15 23:04:07 -0500

    create a form and validations into form

--- Curso_LaWebEmpiezaAqui/AngularConcepts/010_formLayout/src/app/adding-form/adding-form.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/010_formLayout/src/app/adding-form/adding-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-adding-form',
   templateUrl: './adding-form.component.html',
   styleUrls: ['./adding-form.component.sass']
 })
-export class AddingFormComponent {
+export class AddingFormComponent implements OnInit {
+  newForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){ }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm(){
+    this.newForm = this.formBuilder.group({
+      firstName: ['Diego', Validators.required],
+      email: ['',Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      password: ['', Validators.compose([
+        Validators.required, Validators.minLength(8)
+      ])]
+    })
+  }
 }
