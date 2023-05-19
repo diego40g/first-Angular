8bd27ff2644b0da2a869530e67db050faec7281f
diego40g <diegopaznaula@hotmail.com>
2023-05-18 22:54:32 -0500

    use a user model and create a new user

--- Curso_LaWebEmpiezaAqui/AngularConcepts/011_services/src/app/service/users.service.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/011_services/src/app/service/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-
-  constructor() { }
+  usuario: User = new User();
+  constructor() {
+    this.usuario.userId=1;
+    this.usuario.firstname="Diego";
+    this.usuario.lastname="Paz";
+  }
 }
