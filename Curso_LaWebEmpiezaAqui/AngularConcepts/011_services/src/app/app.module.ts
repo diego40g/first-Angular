3edcd757480d81492c7e2cd28b10be4d6e33c158
diego40g <diegopaznaula@hotmail.com>
2023-05-18 22:53:59 -0500

    adding user service into providers

--- Curso_LaWebEmpiezaAqui/AngularConcepts/011_services/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/011_services/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersService } from './service/users.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
