f89bfa31e82fd17b13497cd71cdecd14a473c4df
diego40g <diegopaznaula@hotmail.com>
2023-06-12 23:34:46 -0500

    adding model of department and employee

---
+++ Angular12-Postgres/BackEndNet/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEndNet.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoingin { get; set; }
+        public string PhotoFieName { get; set; }
+    }
+}
