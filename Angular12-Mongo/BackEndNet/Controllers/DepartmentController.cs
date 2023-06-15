14cd6442272859ba1a7a24b58413c04e95b9dbae
diego40g <diegopaznaula@hotmail.com>
2023-06-14 23:33:02 -0500

    creating http reques CRUD of employees and departments

---
+++ Angular12-Mongo/BackEndNet/Controllers/DepartmentController.cs
@@ -0,0 +1,73 @@
+﻿using BackEndNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Data;
+
+namespace BackEndNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var dbList = dbClient.GetDatabase("angularMongo").GetCollection<Department>("Department").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            int LastDepartmentId = dbClient.GetDatabase("angularMongo").GetCollection<Department>("Department").AsQueryable().Count();
+            dep.DepartmentId = LastDepartmentId + 1;
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Department>("Department").InsertOne(dep);
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", dep.DepartmentId);
+
+            var update = Builders<Department>.Update.Set("DepartmentName", dep.DepartmentName);
+
+
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Department>("Department").UpdateOne(filter, update);
+
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Department>("Department").DeleteOne(filter);
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ Angular12-Mongo/BackEndNet/Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using BackEndNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace BackEndNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var dbList = dbClient.GetDatabase("angularMongo").GetCollection<Employee>("Employee").AsQueryable();
+
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            int LastEmployeeId = dbClient.GetDatabase("angularMongo").GetCollection<Department>("Employee").AsQueryable().Count();
+            emp.EmployeeId = LastEmployeeId + 1;
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Employee>("Employee").InsertOne(emp);
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var filter = Builders<Employee>.Filter.Eq("EmployeeId", emp.EmployeeId);
+
+            var update = Builders<Employee>.Update.Set("EmployeeName", emp.EmployeeName)
+                                                    .Set("Department", emp.Department)
+                                                    .Set("DateOfJoining", emp.DateOfJoining)
+                                                    .Set("PhotoFileName", emp.PhotoFileName);
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Employee>("Employee").UpdateOne(filter, update);
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("DB_Conn"));
+
+            var filter = Builders<Employee>.Filter.Eq("EmployeeId", id);
+
+
+            dbClient.GetDatabase("angularMongo").GetCollection<Employee>("Employee").DeleteOne(filter);
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("anonymous.png");
+            }
+        }
+    }
+}
