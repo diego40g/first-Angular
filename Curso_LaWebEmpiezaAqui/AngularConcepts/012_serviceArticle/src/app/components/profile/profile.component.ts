1601d1fc7045a67605dbd153a0696900816f636d
diego40g <diegopaznaula@hotmail.com>
2023-06-24 23:49:26 -0500

    adding component to manage profile

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/012_serviceArticle/src/app/components/profile/profile.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.sass'],
+})
+export class ProfileComponent {
+  constructor(public InjectUser: UserService) {}
+  changeName() {
+    this.InjectUser.usuario.firstname = 'Ignacio';
+  }
+}
