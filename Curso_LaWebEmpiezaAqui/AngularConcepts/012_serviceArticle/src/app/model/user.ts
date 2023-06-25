0eeed6c5ce24b777ee33687195b28b0f07683234
diego40g <diegopaznaula@hotmail.com>
2023-06-24 23:50:46 -0500

    adding user and article model

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/012_serviceArticle/src/app/model/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  userId?: number;
+  firstname?: string;
+  lastname?: string;
+  password?: string;
+
+  constructor() {
+    this.userId = this.userId;
+    this.firstname = this.firstname;
+    this.lastname = this.lastname;
+    this.password = this.password;
+  }
+}
