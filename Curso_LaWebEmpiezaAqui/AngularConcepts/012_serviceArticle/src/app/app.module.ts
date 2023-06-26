eaa22eead27a8df75a013e9ddf506745bc2883d9
diego40g <diegopaznaula@hotmail.com>
2023-06-25 23:37:30 -0500

    adding the new componets and services

--- Curso_LaWebEmpiezaAqui/AngularConcepts/012_serviceArticle/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/012_serviceArticle/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DetailArticleComponent } from './components/detail-article/detail-article.component';
+import { UserService } from './service/user.service';
+import { ArticleService } from './service/article.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProfileComponent,
+    DetailArticleComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [UserService, ArticleService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
