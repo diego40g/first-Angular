af61c2e8dfc6a2fb83bb6cb326f7e4da0f2ef161
diego40g <diegopaznaula@hotmail.com>
2023-06-26 23:52:21 -0500

    adding get request of articles

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpGet/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpGet/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DetailArticleComponent } from './components/detail-article/detail-article.component';
+import { UserService } from './service/user.service';
+import { ArticleService } from './service/article.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProfileComponent,
+    DetailArticleComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [UserService, ArticleService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
