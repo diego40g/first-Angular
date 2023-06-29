b90774767ace0f390ea76326fb4da06b6ed7f9d5
diego40g <diegopaznaula@hotmail.com>
2023-06-28 22:55:32 -0500

    adding components and services

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/manageUser/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/manageUser/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArticleComponent } from './components/article/article.component';
+import { UserService } from './services/user.service';
+import { ArticleService } from './services/article.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HomeComponent, ArticleComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ArticleService, UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
