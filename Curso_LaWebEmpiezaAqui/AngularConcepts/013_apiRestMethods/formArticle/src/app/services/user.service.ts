145c7864b1380005dfc031c52dfdbd84702683d0
diego40g <diegopaznaula@hotmail.com>
2023-06-30 23:36:44 -0500

    adding a service to get all users and specific user using get request

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/formArticle/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  users: User = new User();
+  constructor(private http: HttpClient) {}
+
+  readUser(userId: number): Observable<User> {
+    return this.http.get<User>(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+  }
+
+  allsUsers(): Observable<User[]> {
+    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users/');
+  }
+}
