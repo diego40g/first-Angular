ec674bd58985249a47c512dddef7e895980622c8
diego40g <diegopaznaula@hotmail.com>
2023-06-30 23:38:03 -0500

    adding a new components, service and import http and forms

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/formArticle/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/formArticle/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleService } from './services/article.service';
+import { UserService } from './services/user.service';
+import { FormArticleComponent } from './components/form-article/form-article.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArticleComponent,
+    FormArticleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ArticleService, UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
