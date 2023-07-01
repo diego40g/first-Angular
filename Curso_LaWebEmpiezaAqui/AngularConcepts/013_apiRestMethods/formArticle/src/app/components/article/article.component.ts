8be65a54912a386a206a62a372fdb63e888995da
diego40g <diegopaznaula@hotmail.com>
2023-06-30 23:35:05 -0500

    adding component to show details of articles

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/formArticle/src/app/components/article/article.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Article } from 'src/app/models/article';
+import { User } from 'src/app/models/user';
+import { ArticleService } from 'src/app/services/article.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.sass'],
+})
+export class ArticleComponent {
+  article: Article = new Article();
+  user: User = new User();
+  constructor(
+    private InjectArticle: ArticleService,
+    private InjectUser: UserService
+  ) {
+    this.article = this.InjectArticle.article;
+  }
+
+  ngOnInit() {
+    this.InjectUser.readUser(this.article.userId!).subscribe((userApi) => {
+      this.user = userApi;
+    });
+  }
+}
