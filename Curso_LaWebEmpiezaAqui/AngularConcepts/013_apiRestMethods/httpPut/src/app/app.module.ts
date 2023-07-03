0861e0a0ff5fc6faefcebd4e656cf48b833600dc
diego40g <diegopaznaula@hotmail.com>
2023-07-02 23:45:23 -0500

    create a article component, adding de components and service

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpPut/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpPut/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormArticleComponent } from './components/form-article/form-article.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ArticleService } from './services/article.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    HomeComponent,
+    FormArticleComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ArticleService, UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
