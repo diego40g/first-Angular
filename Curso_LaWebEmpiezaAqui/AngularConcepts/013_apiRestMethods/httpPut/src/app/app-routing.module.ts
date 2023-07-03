b2525615e7e389b7384711d1b20a6187107f62e2
diego40g <diegopaznaula@hotmail.com>
2023-07-02 23:43:26 -0500

    adding path of routes to home add articles and show

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpPut/src/app/app-routing.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/httpPut/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormArticleComponent } from './components/form-article/form-article.component';
+import { ArticleComponent } from './components/article/article.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'article',
+    component: ArticleComponent,
+  },
+  {
+    path: 'add-article',
+    component: FormArticleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
