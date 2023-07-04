6a67a1e92d0baed07ea5ded4994c445efafece10
diego40g <diegopaznaula@hotmail.com>
2023-07-03 23:18:53 -0500

    adding paths of web pages

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/recoveryDataForm/src/app/app-routing.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/recoveryDataForm/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FormArticleComponent } from './components/form-article/form-article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'article',
+    component: ArticleComponent,
+  },
+  {
+    path: 'add-article/:isNew',
+    component: FormArticleComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
