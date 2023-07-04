b86a1981c1ef23a0d282993a146f71afdec475f5
diego40g <diegopaznaula@hotmail.com>
2023-07-03 23:55:06 -0500

    adding analitycs and components, services

--- Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/recoveryDataForm/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/013_apiRestMethods/recoveryDataForm/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FormArticleComponent } from './components/form-article/form-article.component';
+import { UserService } from './services/user.service';
+import { ArticleService } from './services/article.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ArticleComponent,
+    FormArticleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ArticleService, UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
