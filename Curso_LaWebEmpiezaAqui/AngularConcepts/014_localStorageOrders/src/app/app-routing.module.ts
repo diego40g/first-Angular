4d97694701c483da0318a28410d51c6e24c204ef
diego40g <diegopaznaula@hotmail.com>
2023-07-05 22:59:22 -0500

    adding routes article, client and product

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/app-routing.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './component/client/client.component';
+import { ProductComponent } from './component/product/product.component';
+import { OrderComponent } from './component/order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientComponent,
+  },
+  {
+    path: 'product',
+    component: ProductComponent,
+  },
+  {
+    path: 'order',
+    component: OrderComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
