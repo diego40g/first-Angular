d6e41ee2767038b86106ca00d5f16f36602c5544
diego40g <diegopaznaula@hotmail.com>
2023-07-06 23:53:44 -0500

    basic functions to use localstorage

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/client/client.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/client/client.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.sass']
+  styleUrls: ['./client.component.sass'],
 })
 export class ClientComponent {
-
+  saveClient() {
+    localStorage.setItem('name', 'Diego');
+  }
 }
