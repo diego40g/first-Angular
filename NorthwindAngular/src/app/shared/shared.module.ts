89000cf0dd1cbe8bb0acdf75721b68d7bccb4b37
diego40g <diegopaznaula@hotmail.com>
2023-08-11 23:54:13 -0500

    export a navbar component

--- NorthwindAngular/src/app/shared/shared.module.ts
+++ NorthwindAngular/src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavBarComponent
+  ],
+  exports: [
+    NavBarComponent
+  ],
   imports: [
     CommonModule
   ]
