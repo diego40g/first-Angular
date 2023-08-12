c33def1b3421d47852e4c88dc02d9f70c30c01cc
diego40g <diegopaznaula@hotmail.com>
2023-08-12 00:44:17 -0500

    adding a empty component customer list into customer

--- NorthwindAngular/src/app/customer/customer.module.ts
+++ NorthwindAngular/src/app/customer/customer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomerListComponent
+  ],
   imports: [
     CommonModule
   ]
