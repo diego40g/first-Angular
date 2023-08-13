4d3ac5d9fe031a2619698883005d72da8a44ecdd
diego40g <diegopaznaula@hotmail.com>
2023-08-12 23:32:17 -0500

    adding a modules components

--- NorthwindAngular/src/app/app.module.ts
+++ NorthwindAngular/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CustomerModule } from './customer/customer.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    CustomerModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
