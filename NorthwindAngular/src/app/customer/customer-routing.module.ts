32b79ea47f5e8a6197209cb77c213b31eb0b6f8c
diego40g <diegopaznaula@hotmail.com>
2023-08-13 14:12:43 -0500

    adding a route of customer list and styles

---
+++ NorthwindAngular/src/app/customer/customer-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+const customerRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path:'',
+        component: CustomerListComponent
+      }
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(customerRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule { }
