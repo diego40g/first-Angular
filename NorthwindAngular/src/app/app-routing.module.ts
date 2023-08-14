d1d99f45aaab81a3c0b03c10ec06ebfd6837ecca
diego40g <diegopaznaula@hotmail.com>
2023-08-14 02:14:23 -0500

    adding router paths

--- NorthwindAngular/src/app/app-routing.module.ts
+++ NorthwindAngular/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(h => h.HomeModule),
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./customer/customer.module').then(c => c.CustomerModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
