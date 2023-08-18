2ce3acf28534f85ae7cfb9e01e20d5524d30ce45
diego40g <diegopaznaula@hotmail.com>
2023-08-18 01:29:12 -0500

    adding a form manager and some validations

--- NorthwindAngular/src/app/login/login.component.ts
+++ NorthwindAngular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   loginError = '';
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildLoginForm();
+  }
+
+  buildLoginForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+    });
+  }
+
+  login(submittedForm: FormGroup) {}
 }
