4a4057f877ec0aac3ef0bfc23cf8abe35df2050d
diego40g <diegopaznaula@hotmail.com>
2023-08-17 23:46:23 -0500

    adding login component

--- NorthwindAngular/src/app/app.module.ts
+++ NorthwindAngular/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CustomerModule } from './customer/customer.module';
 import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
