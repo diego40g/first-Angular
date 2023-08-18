2cbf0d3c44054410f975a24f2b00bdee01fdcdba
diego40g <diegopaznaula@hotmail.com>
2023-08-17 23:48:03 -0500

    adding a path to manage the login

--- NorthwindAngular/src/app/app-routing.module.ts
+++ NorthwindAngular/src/app/app-routing.module.ts
@@ -1,29 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(h => h.HomeModule),
+    loadChildren: () => import('./home/home.module').then((h) => h.HomeModule),
   },
   {
     path: 'customer',
-    loadChildren: () => import('./customer/customer.module').then(c => c.CustomerModule),
+    loadChildren: () =>
+      import('./customer/customer.module').then((c) => c.CustomerModule),
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
   {
     path: '**',
-    component: NotFoundComponent
-  }
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
