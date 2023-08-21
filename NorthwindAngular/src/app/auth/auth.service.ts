453909f412c3b83d5f80cca284619bcb31718ae7
diego40g <diegopaznaula@hotmail.com>
2023-08-20 22:47:55 -0500

    extends and implements chage methods when logout clean data

--- NorthwindAngular/src/app/auth/auth.service.ts
+++ NorthwindAngular/src/app/auth/auth.service.ts
@@ -11,18 +11,25 @@ import { environment } from 'src/environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import jwt_decode from 'jwt-decode';
 import { transformError } from '../common/common';
+import { CacheService } from './cache.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthService {
+export class AuthService extends CacheService {
   private readonly authProvider: (
     email: string,
     password: string
   ) => Observable<IServerAuthResponse>;
-  authStatus = new BehaviorSubject<IAuthStatus>(defaulAuthStatus);
+  authStatus = new BehaviorSubject<IAuthStatus>(
+    this.getItem('authStatus') || defaulAuthStatus
+  );
 
   constructor(private httpClient: HttpClient) {
+    super();
+    this.authStatus.subscribe((authStatus) => {
+      this.setItem('authStatus', authStatus);
+    });
     this.authProvider = this.userAuthProvider;
   }
   private userAuthProvider(
@@ -40,6 +47,7 @@ export class AuthService {
 
     const loginResponse = this.authProvider(email, password).pipe(
       map((value) => {
+        this.setToken(value.access_Token);
         const result = jwt_decode(value.access_Token);
         return result as IAuthStatus;
       }),
@@ -57,7 +65,20 @@ export class AuthService {
     return loginResponse;
   }
   logout() {
+    this.clearToken();
     this.authStatus.next(defaulAuthStatus);
+  }
+
+  private setToken(jwt: string) {
+    this.setItem('jwt', jwt);
+  }
+
+  getToken(): string {
+    return this.getItem('jwt') || '';
+  }
+
+  private clearToken() {
+    this.removeItem('jwt');
   }
 }
 
--- NorthwindAngular/src/app/login/login.component.ts
+++ NorthwindAngular/src/app/login/login.component.ts
@@ -19,6 +19,7 @@ export class LoginComponent {
   ) {}
 
   ngOnInit() {
+    this.authService.logout();
     this.buildLoginForm();
   }
 
