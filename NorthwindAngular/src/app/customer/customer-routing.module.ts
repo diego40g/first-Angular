54444b9ce2b1042cbb1cfd4b95641a5a5ddb1521
diego40g <diegopaznaula@hotmail.com>
2023-08-23 00:51:06 -0500

    adding canLoad and canActivate in the roputes

--- NorthwindAngular/src/app/customer/customer-routing.module.ts
+++ NorthwindAngular/src/app/customer/customer-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { AuthGuard } from '../auth/auth.guard';
+import { Role } from '../auth/role';
 
 const customerRoutes: Routes = [
   {
     path: 'customer',
+    canLoad: [AuthGuard],
     children: [
       {
         path: '',
         component: CustomerListComponent,
       },
     ],
+    canActivate: [AuthGuard],
+    data: { expectedRole: Role.AdminSupplier },
   },
 ];
 
--- NorthwindAngular/src/app/home/home-routing.module.ts
+++ NorthwindAngular/src/app/home/home-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const homeRoutes: Routes = [
   {
     path: 'home',
+    canLoad: [AuthGuard],
     children: [
       {
         path: '',
