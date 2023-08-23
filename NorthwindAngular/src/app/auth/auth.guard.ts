b49ab9c080fb70e373d9c53b3f3179c72113d1ed
diego40g <diegopaznaula@hotmail.com>
2023-08-22 23:55:30 -0500

    Adding auth guard to manage private paths

--- NorthwindAngular/src/app/auth/auth.guard.ts
+++ NorthwindAngular/src/app/auth/auth.guard.ts
@@ -1,8 +1,51 @@
-import { CanActivateFn, CanActivateChildFn, CanLoadFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  Router,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService, IAuthStatus } from './auth.service';
 
-export const AuthGuard: CanActivateFn | CanActivateChildFn | CanLoadFn = (
-  route,
-  state
-) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  constructor(private authService: AuthService, private route: Router) {}
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    throw new Error('Method not implemented.');
+  }
+  canLoad(route: Route): boolean | Observable<boolean> | Promise<boolean> {
+    return this.checkLogin();
+  }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+
+  protected checkLogin(): boolean {
+    if (
+      this.authService.getToken() == null ||
+      this.authService.getToken() === ''
+    ) {
+      alert('You must login to continue');
+      this.route.createUrlTree(['login']);
+      return false;
+    }
+    return true;
+  }
+}
