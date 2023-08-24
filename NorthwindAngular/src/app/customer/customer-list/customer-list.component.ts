cfa918d9367fd1bab9a08c0e2e6b307f22986a7d
diego40g <diegopaznaula@hotmail.com>
2023-08-24 01:08:29 -0500

    adding style stuct and logic for customer list

--- NorthwindAngular/src/app/customer/customer-list/customer-list.component.ts
+++ NorthwindAngular/src/app/customer/customer-list/customer-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CustomerService } from './customer.service';
+import { Customer } from '../models/customer';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.sass']
+  styleUrls: ['./customer-list.component.sass'],
+  providers: [CustomerService],
 })
 export class CustomerListComponent {
+  customers: Customer[] = [];
+  constructor(private customerServie: CustomerService) {
+    this.getCustomer(1, 20);
+  }
 
+  getCustomer(page: number, rows: number): void {
+    this.customerServie
+      .getCustomerList(page, rows)
+      .subscribe((response) => (this.customers = response));
+  }
 }
