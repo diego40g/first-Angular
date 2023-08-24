c4d469490facf5901067fef57d6b79eb94f6a123
diego40g <diegopaznaula@hotmail.com>
2023-08-24 01:10:01 -0500

    import a angular material necesary

--- NorthwindAngular/src/app/customer/customer.module.ts
+++ NorthwindAngular/src/app/customer/customer.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerRoutingModule } from './customer-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    CustomerListComponent
-  ],
+  declarations: [CustomerListComponent],
   imports: [
     CommonModule,
-    CustomerRoutingModule
-  ]
+    CustomerRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
-export class CustomerModule { }
+export class CustomerModule {}
