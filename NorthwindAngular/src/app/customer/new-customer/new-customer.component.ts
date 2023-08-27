d390b3d819861080ff3bbd0ecd33080018e4a94a
diego40g <diegopaznaula@hotmail.com>
2023-08-26 23:19:59 -0500

    adding new component for generate new customer with popup

---
+++ NorthwindAngular/src/app/customer/new-customer/new-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrls: ['./new-customer.component.sass']
+})
+export class NewCustomerComponent {
+  saveCustomer(): void{
+
+  }
+}
