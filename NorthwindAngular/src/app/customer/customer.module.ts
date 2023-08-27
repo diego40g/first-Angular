165a7435c5f0adf7da23d723a8e5bb6b63d155ad
diego40g <diegopaznaula@hotmail.com>
2023-08-26 23:21:33 -0500

    adding new Customer and the import of dialog module

--- NorthwindAngular/src/app/customer/customer.module.ts
+++ NorthwindAngular/src/app/customer/customer.module.ts
@@ -5,15 +5,18 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, NewCustomerComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatPaginatorModule,
-  ],
+    MatDialogModule
+  ]
 })
 export class CustomerModule {}
