aff2f65b062ce9d7fe4fa092684020ee37fe8526
diego40g <diegopaznaula@hotmail.com>
2023-08-27 23:58:22 -0500

    adding a funtions validations and delting the inused class for styles

--- NorthwindAngular/src/app/customer/new-customer/new-customer.component.ts
+++ NorthwindAngular/src/app/customer/new-customer/new-customer.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AddOrEditCustomer } from '../models/add-or-edit-customer';
+import { WhiteSpaceValidator } from 'src/app/shared/validators/white-space-validator';
 
 @Component({
   selector: 'app-new-customer',
   templateUrl: './new-customer.component.html',
-  styleUrls: ['./new-customer.component.sass']
+  styleUrls: ['./new-customer.component.sass'],
 })
 export class NewCustomerComponent {
-  saveCustomer(): void{
+  newCustomerForm!: FormGroup;
+  customer?: AddOrEditCustomer;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildNewCustomerForm();
   }
+
+  buildNewCustomerForm(): void {
+    this.newCustomerForm = this.fb.group({
+      firstName: [
+        '',
+        [Validators.required, WhiteSpaceValidator.cannotContainSpace],
+      ],
+      lastName: [
+        '',
+        [Validators.required, WhiteSpaceValidator.cannotContainSpace],
+      ],
+      city: ['', [Validators.required, WhiteSpaceValidator.cannotContainSpace]],
+      country: [
+        '',
+        [Validators.required, WhiteSpaceValidator.cannotContainSpace],
+      ],
+      phone: [
+        '',
+        [Validators.required, WhiteSpaceValidator.cannotContainSpace],
+      ],
+    });
+  }
+  save(): void {}
 }
