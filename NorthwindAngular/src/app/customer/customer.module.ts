84f60d51fb42e63a89bf657be31b91554c36f348
diego40g <diegopaznaula@hotmail.com>
2023-08-27 23:50:15 -0500

    import a necesary lib to angular material and forms to manage forms

--- NorthwindAngular/src/app/customer/customer.module.ts
+++ NorthwindAngular/src/app/customer/customer.module.ts
@@ -7,16 +7,23 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { NewCustomerComponent } from './new-customer/new-customer.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [CustomerListComponent, NewCustomerComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     CustomerRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatPaginatorModule,
-    MatDialogModule
-  ]
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class CustomerModule {}
