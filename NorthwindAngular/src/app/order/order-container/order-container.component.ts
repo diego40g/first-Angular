9998fc20e5fa8a55a3a635f0a19868083b3e3305
diego40g <diegopaznaula@hotmail.com>
2023-09-01 01:35:50 -0500

    adding a component to generate a table

--- NorthwindAngular/src/app/order/order-container/order-container.component.ts
+++ NorthwindAngular/src/app/order/order-container/order-container.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { OrderContainerService } from './order-container.service';
+import { OrderList } from '../models/order-list';
+import { Column } from 'src/app/shared/table-view/table-view.component';
 
 @Component({
   selector: 'app-order-container',
   templateUrl: './order-container.component.html',
-  styleUrls: ['./order-container.component.sass']
+  styleUrls: ['./order-container.component.sass'],
+  providers: [OrderContainerService],
 })
 export class OrderContainerComponent {
+  items: OrderList[] = [];
+  numberOfRecords? = 0;
+  columns: Column[] = [];
 
+  constructor(private service: OrderContainerService) {}
+
+  ngOnInit() {
+    this.getOrders(1, 10);
+    this.columns = this.getColumns();
+  }
+
+  getOrders(page: number, rows: number): void {
+    this.service.getOrderList(page, rows).subscribe((response) => {
+      this.items = response;
+      this.numberOfRecords = response[0].totalRecords;
+    });
+  }
+
+  private getColumns(): Column[] {
+    return [
+      {
+        name: 'Id',
+        flexGrow: 0.5,
+        prop: 'orderId',
+      },
+      {
+        name: 'Customer',
+        prop: 'customer',
+        flexGrow: 1,
+      },
+      {
+        name: 'Total',
+        prop: 'totalAmount',
+        flexGrow: 0.5,
+      },
+      {
+        name: '# Order',
+        prop: 'orderNumber',
+        flexGrow: 0.5,
+      },
+    ];
+  }
 }
