e53aefc223634ce32237c1bb9d7a560147ca9cc6
diego40g <diegopaznaula@hotmail.com>
2023-09-01 01:35:06 -0500

    export a generic table

--- NorthwindAngular/src/app/shared/shared.module.ts
+++ NorthwindAngular/src/app/shared/shared.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CustomerModule } from '../customer/customer.module';
-import { HomeModule } from '../home/home.module';
+import { TableViewComponent } from './table-view/table-view.component';
+
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
-  declarations: [NavBarComponent, NotFoundComponent],
-  exports: [NavBarComponent],
-  imports: [CommonModule, RouterModule, CustomerModule, HomeModule],
+  declarations: [NavBarComponent, NotFoundComponent, TableViewComponent],
+  exports: [NavBarComponent, NotFoundComponent, TableViewComponent],
+  imports: [CommonModule, RouterModule, NgxDatatableModule],
 })
 export class SharedModule {}
