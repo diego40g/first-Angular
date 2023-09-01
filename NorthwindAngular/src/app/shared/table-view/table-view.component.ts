988f02274fdc885c6ee3e98f1011d7e35cee0429
diego40g <diegopaznaula@hotmail.com>
2023-09-01 01:34:29 -0500

    adding new component to generate a generic table

---
+++ NorthwindAngular/src/app/shared/table-view/table-view.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+
+export interface Column {
+  flexGrow: number;
+  name: string;
+  resizeable?: any;
+  cellTemplate?: any;
+  prop: string;
+  sortable?: any;
+}
+
+@Component({
+  selector: 'app-table-view',
+  templateUrl: './table-view.component.html',
+  styleUrls: ['./table-view.component.sass'],
+})
+export class TableViewComponent {
+  @Input() minTableHeight: number = 500;
+  @Input() items: object[] = [];
+  @Input() columns: Column[] = [];
+  @Input() limit?: number;
+  @Input() detailTemplate!: TemplateRef<any>;
+
+  @ViewChild('myTable') table: any;
+
+  toggleExpandRow(row: number) {
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+}
