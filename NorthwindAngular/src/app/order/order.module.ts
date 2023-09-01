50b23428262a5aa2fefcd5e9d8a8b00fb095b428
diego40g <diegopaznaula@hotmail.com>
2023-09-01 01:35:34 -0500

    import a shared moduleto use generic table

--- NorthwindAngular/src/app/order/order.module.ts
+++ NorthwindAngular/src/app/order/order.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderContainerComponent } from './order-container/order-container.component';
-
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    OrderContainerComponent
-  ],
-  imports: [
-    CommonModule,
-    OrderRoutingModule
-  ]
+  declarations: [OrderContainerComponent],
+  imports: [CommonModule, OrderRoutingModule, SharedModule],
 })
-export class OrderModule { }
+export class OrderModule {}
