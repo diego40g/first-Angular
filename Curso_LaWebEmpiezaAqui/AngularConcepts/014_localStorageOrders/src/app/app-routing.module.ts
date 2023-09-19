b4fd3625e77b93f5d6ab131b657f30aeede72b15
diego40g <diegopaznaula@hotmail.com>
2023-09-18 23:07:43 -0500

    updating a navbar with the new items add client and home

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/app-routing.module.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/app-routing.module.ts
@@ -3,10 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './component/client/client.component';
 import { ProductComponent } from './component/product/product.component';
 import { OrderComponent } from './component/order/order.component';
+import { HomeComponent } from './component/home/home.component';
+import { AddClientComponent } from './component/add-client/add-client.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'addclient',
+    component: AddClientComponent,
+  },
+  {
+    path: 'client',
     component: ClientComponent,
   },
   {
