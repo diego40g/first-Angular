37ea15d85e8886eb6572a1b9f8c5bb1b35fb1f00
diego40g <diegopaznaula@hotmail.com>
2023-09-19 21:27:29 -0500

    adding config for use adding form

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/add-client/add-client.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/add-client/add-client.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-client',
   templateUrl: './add-client.component.html',
-  styleUrls: ['./add-client.component.sass']
+  styleUrls: ['./add-client.component.sass'],
 })
 export class AddClientComponent {
+  addForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.addForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      address: ['', Validators.required],
+    });
+  }
 }
