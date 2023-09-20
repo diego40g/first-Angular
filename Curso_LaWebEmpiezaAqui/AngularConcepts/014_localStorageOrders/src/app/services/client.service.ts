b8a69e124e4fb8961f317b3c49dcf326068c92f0
diego40g <diegopaznaula@hotmail.com>
2023-09-19 21:56:53 -0500

    adding servie to add and get clients

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/services/client.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Client } from '../models/client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClientService {
+  constructor() {}
+
+  addLocalStorage(client: Client) {
+    let oldClients: Client[] = this.clientsLocalStorage;
+    oldClients.push(client);
+    localStorage.setItem('clients', JSON.stringify(oldClients));
+  }
+
+  get clientsLocalStorage(): Client[] {
+    let clientsOfLocalStorage: Client[] | null = JSON.parse(
+      localStorage.getItem('clients')!
+    );
+    if (clientsOfLocalStorage == null) {
+      return new Array<Client>();
+    }
+    return clientsOfLocalStorage;
+  }
+}
