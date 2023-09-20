76bd7b1846f2520fb9183af030dc30f94f55a50d
diego40g <diegopaznaula@hotmail.com>
2023-09-19 21:37:36 -0500

    adding struct to client

---
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/models/client.ts
@@ -0,0 +1,13 @@
+export class Client {
+  clientId?: number;
+  firstname?: string;
+  lastname?: string;
+  address?: string;
+
+  constructor() {
+    this.clientId = this.clientId;
+    this.firstname = this.firstname;
+    this.lastname = this.lastname;
+    this.address = this.address;
+  }
+}
