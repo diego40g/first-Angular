512b5d9177c7f6bca398e2ad8ed9390530397efc
diego40g <diegopaznaula@hotmail.com>
2023-09-20 22:47:59 -0500

    adding function to search a client

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/home/home.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/home/home.component.ts
@@ -14,4 +14,21 @@ export class HomeComponent {
   ngOnInit() {
     this.clients = this.clientsService.clientsLocalStorage;
   }
+
+  searchClient(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const searchWord: string = target.value;
+    this.clients = this.clientsService.clientsLocalStorage.filter((client) => {
+      const searchFirstName = client.firstname?.toLocaleLowerCase() || '';
+      const searchLastName = client.lastname?.toLocaleLowerCase() || '';
+      const searchAddress = client.address?.toLocaleLowerCase() || '';
+
+      return (
+        searchFirstName.includes(searchWord.toLowerCase()) ||
+        searchLastName.includes(searchWord.toLowerCase()) ||
+        searchAddress.includes(searchWord.toLowerCase()) ||
+        client.clientId?.toString().includes(searchWord.toLowerCase())
+      );
+    });
+  }
 }
