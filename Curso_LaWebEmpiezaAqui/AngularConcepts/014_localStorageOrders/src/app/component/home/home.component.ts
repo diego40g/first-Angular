e1c8b74d8e0682410561b71cd68527659b8f7b10
diego40g <diegopaznaula@hotmail.com>
2023-09-20 22:27:18 -0500

    manage clients with service and ng for on html

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/home/home.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Client } from 'src/app/models/client';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent {
+  clients: Array<Client> = new Array<Client>();
+  constructor(private clientsService: ClientService) {}
 
+  ngOnInit() {
+    this.clients = this.clientsService.clientsLocalStorage;
+  }
 }
