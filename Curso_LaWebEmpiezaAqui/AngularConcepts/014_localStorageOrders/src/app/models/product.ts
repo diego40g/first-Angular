3cfc1f370d4b001b5990ef7bd0558882801772a0
diego40g <diegopaznaula@hotmail.com>
2023-09-22 22:50:25 -0500

    adding a model to product

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/models/product.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/models/product.ts
@@ -1,2 +1,13 @@
 export class Product {
+  productId?: number;
+  name?: string;
+  description?: string;
+  price?: number;
+
+  constructor() {
+    this.productId = this.productId;
+    this.name = this.name;
+    this.description = this.description;
+    this.price = this.price;
+  }
 }
