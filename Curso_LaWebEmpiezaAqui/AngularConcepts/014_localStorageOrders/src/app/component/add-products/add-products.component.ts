c611709fe3e909600038b17f2bb94bc2db4a4ff9
diego40g <diegopaznaula@hotmail.com>
2023-09-22 22:49:57 -0500

    adding a form to create product

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/add-products/add-products.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/add-products/add-products.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
   styleUrls: ['./add-products.component.sass'],
 })
-export class AddProductsComponent {}
+export class AddProductsComponent {
+  addForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.addForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+    });
+  }
+
+  addProduct() {}
+}
