5bc4ce15da2ad64fb8a5cc4f7cea5bdb416f60c2
diego40g <diegopaznaula@hotmail.com>
2023-09-22 22:50:54 -0500

    the view to list all products

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/product/product.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/product/product.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.sass']
+  styleUrls: ['./product.component.sass'],
 })
 export class ProductComponent {
-
+  products: Array<Product> = new Array<Product>();
+  searchproduct(event: Event) {}
 }
