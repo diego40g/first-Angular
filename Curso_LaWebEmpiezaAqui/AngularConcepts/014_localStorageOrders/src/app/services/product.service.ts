18a07df61c5b575d564243df90459794dba255b1
diego40g <diegopaznaula@hotmail.com>
2023-09-23 21:51:43 -0500

    adding a product service for localstorage

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/services/product.service.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor() {}
 
-  constructor() { }
+  addingLocalStorage(product: Product) {
+    let oldProduct: Product[] = this.productsLocalStorage;
+    product.productId = oldProduct.length + 1;
+    oldProduct.push(product);
+    localStorage.setItem('products', JSON.stringify(oldProduct));
+  }
+
+  get productsLocalStorage(): Product[] {
+    let productToLocalStorage: Product[] | null = JSON.parse(
+      localStorage.getItem('products')!
+    );
+    if (productToLocalStorage == null) {
+      return new Array<Product>();
+    }
+    return productToLocalStorage;
+  }
 }
