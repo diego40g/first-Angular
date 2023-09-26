c6d9c59d8aa9289631b63f11fbaac0199c34d27e
diego40g <diegopaznaula@hotmail.com>
2023-09-25 22:04:31 -0500

    first order with details order

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/order/order.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/order/order.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Order } from 'src/app/models/order';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.sass']
+  styleUrls: ['./order.component.sass'],
 })
 export class OrderComponent {
-
+  constructor() {}
+  ngOnInit() {
+    let pedido: Order = new Order();
+    pedido.pedidoId = 1;
+    pedido.clientId = 1;
+    pedido.firstnameClient = 'Diego';
+    pedido.total = 1442;
+    pedido.orderDetail.push(
+      {
+        productId: 1,
+        nameProduct: 'Maíz',
+        stock: 5,
+        price: 15,
+        total: 75,
+      },
+      {
+        productId: 2,
+        nameProduct: 'Agua',
+        stock: 1,
+        price: 20,
+        total: 20,
+      }
+    );
+    console.log(pedido);
+  }
 }
--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/models/order.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/models/order.ts
@@ -1,12 +1,14 @@
 import { OrderDetail } from './order-detail';
 
 export class Order {
+  pedidoId?: number;
   clientId?: number;
   firstnameClient?: string;
   total?: number;
-  orderDetail?: Array<OrderDetail>;
+  orderDetail!: Array<OrderDetail>;
 
   constructor() {
+    this.pedidoId = this.pedidoId;
     this.clientId = this.clientId;
     this.firstnameClient = this.firstnameClient;
     this.total = this.total;
