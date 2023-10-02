29b741b2d41e129cb569c974b34af73973865592
diego40g <diegopaznaula@hotmail.com>
2023-10-01 22:32:52 -0500

    adding material module to use paginator

--- NorthwindAngular/src/app/order/order.module.ts
+++ NorthwindAngular/src/app/order/order.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderContainerComponent } from './order-container/order-container.component';
 import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [OrderContainerComponent],
-  imports: [CommonModule, OrderRoutingModule, SharedModule],
+  imports: [CommonModule, OrderRoutingModule, SharedModule, MaterialModule],
 })
 export class OrderModule {}
