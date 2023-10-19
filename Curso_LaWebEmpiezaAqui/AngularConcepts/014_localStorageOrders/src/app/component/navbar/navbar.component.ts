d3e589290a7a156ee6e8d66898d02aacc2a3aea8
diego40g <diegopaznaula@hotmail.com>
2023-10-18 22:08:01 -0500

    adding order service to show the data (name) of client

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/navbar/navbar.component.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent {
-
+  constructor(public orderService: OrderService) {}
 }
