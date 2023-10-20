4c4ca8c32080ce90a2858b740a819cce72091be5
diego40g <diegopaznaula@hotmail.com>
2023-10-19 22:23:03 -0500

    updating service

--- Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/services/order.service.ts
+++ Curso_LaWebEmpiezaAqui/AngularConcepts/014_localStorageOrders/src/app/services/order.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { Order } from '../models/order';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
   order: Order = new Order();
-  constructor() { }
+  constructor() {}
 }
