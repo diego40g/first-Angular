294ae3f279b8ae6ffb0b721d0d7ec790a3446a9a
diego40g <diegopaznaula@hotmail.com>
2023-10-28 23:13:58 -0500

    importing accordion and browser animation

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
