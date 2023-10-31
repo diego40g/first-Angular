9200d9950ee25571fafadef6d0c6b0cdc90a9af2
diego40g <diegopaznaula@hotmail.com>
2023-10-30 22:07:09 -0500

    import compat for old version

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/app.module.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/app.module.ts
@@ -6,14 +6,22 @@ import { AppComponent } from './app.component';
 
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'; // Utiliza 'compat' para mantener compatibilidad con versiones anteriores
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AuthComponent } from './auth/auth.component';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AuthComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AccordionModule.forRoot(),
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase
+    AngularFirestoreModule, // Módulo para Firestore (base de datos en tiempo real)
+    AngularFireAuthModule, // Módulo para autenticación
   ],
   providers: [],
   bootstrap: [AppComponent],
