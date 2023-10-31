c2cbd7959ccb639bba6fb1d768e89b25534f1045
diego40g <diegopaznaula@hotmail.com>
2023-10-30 22:51:35 -0500

    the valdiation user and password

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/app.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'masterGym';
+  user: Observable<any>;
+
+  constructor(private auth: AngularFireAuth) {
+    this.user = this.auth.authState;
+    this.user.subscribe((user) => {
+      console.log(user);
+    });
+  }
+
+  login() {
+    //this.auth.signInWithPopup(new GoogleAuthProvider());
+    this.auth.signInWithEmailAndPassword('test@test.com', '1234567890');
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
