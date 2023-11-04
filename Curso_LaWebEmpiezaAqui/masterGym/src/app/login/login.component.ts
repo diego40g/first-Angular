2466d64ac2e2c142c0d41d22a5285230c03906fc
diego40g <diegopaznaula@hotmail.com>
2023-11-03 23:46:44 -0500

    updating  component and adding validation with login function

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/login/login.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/login/login.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent {
+  formLogin!: FormGroup;
 
+  constructor(private addForm: FormBuilder, private fbAuth: AngularFireAuth) {}
+
+  ngOnInit() {
+    this.formLogin = this.addForm.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    this.fbAuth
+      .signInWithEmailAndPassword(
+        this.formLogin.value.email,
+        this.formLogin.value.password
+      )
+      .then((user) => {
+        console.log(user);
+      });
+  }
 }
