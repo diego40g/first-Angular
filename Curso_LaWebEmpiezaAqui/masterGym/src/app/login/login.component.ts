725cc97ed17477a0d92217c081e9634e90aceb57
diego40g <diegopaznaula@hotmail.com>
2023-11-07 21:39:24 -0500

    using ngx spinner

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/login/login.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent {
   formLogin!: FormGroup;
   correctData: boolean = true;
-  errorText: string = "";
+  errorText: string = '';
 
-  constructor(private addForm: FormBuilder, private fbAuth: AngularFireAuth) {}
+  constructor(
+    private addForm: FormBuilder,
+    private fbAuth: AngularFireAuth,
+    private spinner: NgxSpinnerService
+  ) {}
 
   ngOnInit() {
     this.formLogin = this.addForm.group({
@@ -22,22 +27,30 @@ export class LoginComponent {
   }
 
   login() {
-  if(this.formLogin.valid){
-    this.correctData = true;
-    this.fbAuth
-      .signInWithEmailAndPassword(
-        this.formLogin.value.email,
-        this.formLogin.value.password
-      )
-      .then((user) => {
-        console.log(user);
-      }).catch((error) => {
-        this.correctData=false;
-        this.errorText=error.message;
-      });
-    }else{
+    if (this.formLogin.valid) {
+      this.correctData = true;
+      this.spinner.show();
+      this.fbAuth
+        .signInWithEmailAndPassword(
+          this.formLogin.value.email,
+          this.formLogin.value.password
+        )
+        .then((user) => {
+          console.log(user);
+          setTimeout(() => {
+            this.spinner.hide();
+          }, 2000);
+        })
+        .catch((error) => {
+          this.correctData = false;
+          this.errorText = error.message;
+          setTimeout(() => {
+            this.spinner.hide();
+          }, 2000);
+        });
+    } else {
       this.correctData = false;
-      this.errorText="Please, check of information was correct"
+      this.errorText = 'Please, check of information was correct';
     }
   }
 }
