1236e7ffea2e2a7ec775f22ed82cdcb2283ea85e
diego40g <diegopaznaula@hotmail.com>
2023-11-11 23:34:26 -0500

    adding a login request login and logout

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/navbar/navbar.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/navbar/navbar.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { User } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent {
+  fireUser?: User;
+  user: Observable<any>;
 
+  constructor(private auth: AngularFireAuth) {
+    this.user = this.auth.authState;
+    this.user.subscribe((user) => {
+      this.fireUser = user;
+    });
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
