881c2f6f3e2216b28c9051d6a83a5fa37b335c7f
diego40g <diegopaznaula@hotmail.com>
2023-11-12 23:34:12 -0500

    cleaning code to use the logout on navbar

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/app.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   title = 'masterGym';
   user: Observable<any>;
   fireUser?: User;
-  load: boolean = true;
+  load: boolean = false;
 
   constructor(private auth: AngularFireAuth) {
     this.user = this.auth.authState;
@@ -27,8 +27,5 @@ export class AppComponent {
   login() {
     //this.auth.signInWithPopup(new GoogleAuthProvider());
     this.auth.signInWithEmailAndPassword('test@test.com', '1234567890');
-  }
-  logout() {
-    this.auth.signOut();
   }
 }
