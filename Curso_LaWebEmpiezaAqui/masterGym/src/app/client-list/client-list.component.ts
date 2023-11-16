135e907fb45079b2412ec7d7b90d9e0caf166956
diego40g <diegopaznaula@hotmail.com>
2023-11-15 22:22:03 -0500

    adding a get values of firebase collection

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/client-list/client-list.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/client-list/client-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-client-list',
   templateUrl: './client-list.component.html',
-  styleUrls: ['./client-list.component.sass']
+  styleUrls: ['./client-list.component.sass'],
 })
 export class ClientListComponent {
+  clients = Array<any>();
+  constructor(private db: AngularFirestore) {}
 
+  ngOnInit() {
+    this.db
+      .collection('client')
+      .valueChanges()
+      .subscribe((results) => {
+        this.clients = results;
+      });
+  }
 }
