2e394157e14919d6e5e9ad9ba0ac3a2a815c0ed9
diego40g <diegopaznaula@hotmail.com>
2023-11-16 22:45:08 -0500

    adding path for redirect on navbar

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/app-routing.module.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientListComponent } from './client-list/client-list.component';
+import { ClientAddComponent } from './client-add/client-add.component';
 
 const routes: Routes = [
   {
     path: 'client-list',
     component: ClientListComponent,
+  },
+  {
+    path: 'client-add',
+    component: ClientAddComponent,
   },
 ];
 
