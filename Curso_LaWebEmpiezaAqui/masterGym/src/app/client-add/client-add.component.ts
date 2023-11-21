599bcce11aa7f2464b2b74f309322c959d4bec85
diego40g <diegopaznaula@hotmail.com>
2023-11-20 22:53:34 -0500

    Add client form and validation

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/client-add/client-add.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/client-add/client-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-client-add',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./client-add.component.sass']
 })
 export class ClientAddComponent {
+  clientForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() {
+    this.clientForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['',Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      identificationCard: [''],
+      birthdate: ['',Validators.required],
+      cellphone: [''],
+      imageUrl: ['',Validators.required]
+    });
+  }
 
+  addClient() { 
+    console.log(this.clientForm.value);
+    
+  }
 }
