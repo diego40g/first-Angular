b470e2f5d2f257ec4732dac5093ddd0ce6ae81ed
diego40g <diegopaznaula@hotmail.com>
2023-11-22 23:13:54 -0500

    Add image upload functionality to client-add component

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/client-add/client-add.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/client-add/client-add.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -8,8 +9,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ClientAddComponent {
   clientForm!: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  uploadPercentange: number = 0;
+  constructor(private fb: FormBuilder, private storage: AngularFireStorage) { }
   ngOnInit() {
+    console.log(new Date().getTime().toString());
+    
     this.clientForm = this.fb.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
@@ -25,6 +29,23 @@ export class ClientAddComponent {
 
   addClient() { 
     console.log(this.clientForm.value);
-    
+  }
+
+  addImage(event: any) {
+    const fecha = new Date().getTime().toString();
+    const file = event.target.files[0];
+    const filePath = `clients/${fecha+file.name}`;
+    const reference = this.storage.ref(filePath);
+    const task = reference.put(file);
+    task.then((obj) => {
+      console.log('Image uploaded');
+      reference.getDownloadURL().subscribe((url) => {
+        console.log(url);
+        this.clientForm.get('imageUrl')?.setValue(url);
+      });
+    });
+    task.percentageChanges().subscribe((percentage) => {
+      this.uploadPercentange = parseInt(percentage!.toString());
+    });
   }
 }
