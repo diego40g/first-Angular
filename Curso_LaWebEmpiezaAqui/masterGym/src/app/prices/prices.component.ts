69a192ed1464c821ac4f1602f0ac70971e685347
diego40g <diegopaznaula@hotmail.com>
2023-12-03 13:49:36 -0500

    Add AngularFirestore and MessagesService imports, and implement functionality to add and edit plans

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/prices/prices.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/prices/prices.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessagesService } from '../services/messages.service';
+import { Price } from '../models/price';
 
 @Component({
   selector: 'app-prices',
@@ -8,8 +11,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PricesComponent {
   priceForm!: FormGroup;
+  prices: Price[] = new Array<Price>();
+  isEdit: boolean = false;
+  id: string = "";
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder, 
+    private db: AngularFirestore,
+    private msj: MessagesService,
+  ) { }
 
   ngOnInit() {
     this.priceForm = this.fb.group({
@@ -18,9 +28,51 @@ export class PricesComponent {
       duration: ['', Validators.required],
       durationType: ['', Validators.required],
     });
+
+    this.getPrices();
+  }
+
+  getPrices(){
+    this.db.collection<Price>('prices').get().subscribe((res)=>{
+      this.prices.length = 0;
+      res.docs.forEach((item)=>{
+        let price: Price = item.data() as Price;
+        price.id = item.id;
+        price.ref = item.ref;
+        this.prices.push(price);
+      });
+    });
   }
 
   addPlan() {
-    console.log(this.priceForm.value);
+    this.db.collection<Price>('prices').add(this.priceForm.value).then(()=>{
+      this.msj.messageSuccess("Add","The plan was added successfully");
+      this.priceForm.reset();
+      this.getPrices();
+    }).catch(()=>{
+      this.msj.messageError("Error","There was an error adding the plan");
+    });
+  }
+
+  editPlan(){
+    this.db.doc('prices/'+this.id).update(this.priceForm.value).then(()=>{
+      this.msj.messageSuccess("Edit","The plan was edited successfully");
+      this.priceForm.reset();
+      this.isEdit = false;
+      this.getPrices();
+    }).catch(()=>{
+      this.msj.messageError("Error","There was an error editing the plan");
+    })
+  }
+
+  editPrice(price: Price){
+    this.isEdit = true;
+    this.priceForm.setValue({
+      name: price.name,
+      cost: price.cost,
+      duration: price.duration,
+      durationType: price.durationType,
+    });
+    this.id = price.id!;
   }
 }
