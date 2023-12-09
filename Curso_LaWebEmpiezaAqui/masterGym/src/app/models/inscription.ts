10801c7122d5db9616f30e29431c1a8e21c654c3
diego40g <diegopaznaula@hotmail.com>
2023-12-08 23:54:24 -0500

    Add Inscription model class

---
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/models/inscription.ts
@@ -0,0 +1,11 @@
+import { DocumentReference } from '@angular/fire/compat/firestore';
+
+export class Inscription {
+  dateInscription!: Date;
+  dateFinal!: Date;
+  client!: DocumentReference;
+  price!: DocumentReference;
+  subTotal!: number;
+  iva!: number;
+  total!: number;
+}
