ef1c71b642e477431c7b3b748bc611c382a2f6a8
diego40g <diegopaznaula@hotmail.com>
2023-12-09 22:54:37 -0500

    Add client-related functionality to inscription component

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Inscription } from '../models/inscription';
+import { Client } from '../models/client';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.sass']
+  styleUrls: ['./inscription.component.sass'],
 })
 export class InscriptionComponent {
+  inscription: Inscription = new Inscription();
+  selectedClient: Client = new Client();
 
+  assignClient(client: Client) {
+    this.inscription.client = client.ref;
+    this.selectedClient = client;
+  }
+
+  deleteClient() {
+    this.selectedClient = new Client();
+    this.inscription.client = undefined;
+  }
+
+  saveClient() {
+    console.log(this.inscription);
+  }
 }
