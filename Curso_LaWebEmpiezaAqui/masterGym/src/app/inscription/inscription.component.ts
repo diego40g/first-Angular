8f32bd56c63a8114706fc3bbd65d00d8c95e5167
diego40g <diegopaznaula@hotmail.com>
2023-12-10 23:20:52 -0500

    Add price retrieval functionality to InscriptionComponent

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Inscription } from '../models/inscription';
 import { Client } from '../models/client';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Price } from '../models/price';
 
 @Component({
   selector: 'app-inscription',
@@ -10,6 +12,23 @@ import { Client } from '../models/client';
 export class InscriptionComponent {
   inscription: Inscription = new Inscription();
   selectedClient: Client = new Client();
+  prices: Price[] = new Array<Price>();
+
+  constructor(private db: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.db
+      .collection('prices')
+      .get()
+      .subscribe((result) => {
+        result.docs.forEach((item) => {
+          let price: any = item.data() as Price;
+          price.id = item.id;
+          price.ref = item.ref;
+          this.prices.push(price);
+        });
+      });
+  }
 
   assignClient(client: Client) {
     this.inscription.client = client.ref;
