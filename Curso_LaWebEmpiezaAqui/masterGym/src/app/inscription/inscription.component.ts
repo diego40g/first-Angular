c7e077d8b42a8b7aedee421a02f4578f140b905a
diego40g <diegopaznaula@hotmail.com>
2023-12-16 23:03:46 -0500

    Add logic to calculate final date based on selected price duration type

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription/inscription.component.ts
@@ -51,5 +51,51 @@ export class InscriptionComponent {
     this.inscription.price = this.selectedPrice?.ref!;
 
     this.inscription.dateInscription = new Date();
+
+    if (this.selectedPrice?.durationType == '1') {
+      let day: number = this.selectedPrice?.duration!;
+      let finalDate = new Date(
+        this.inscription.dateInscription.getFullYear(),
+        this.inscription.dateInscription.getMonth(),
+        this.inscription.dateInscription.getDate() + day
+      );
+      this.inscription.dateFinal = finalDate;
+    }
+    if (this.selectedPrice?.durationType == '2') {
+      let day: number = this.selectedPrice?.duration! * 7;
+      let finalDate = new Date(
+        this.inscription.dateInscription.getFullYear(),
+        this.inscription.dateInscription.getMonth(),
+        this.inscription.dateInscription.getDate() + day
+      );
+      this.inscription.dateFinal = finalDate;
+    }
+    if (this.selectedPrice?.durationType == '3') {
+      let day: number = this.selectedPrice?.duration! * 15;
+      let finalDate = new Date(
+        this.inscription.dateInscription.getFullYear(),
+        this.inscription.dateInscription.getMonth(),
+        this.inscription.dateInscription.getDate() + day
+      );
+      this.inscription.dateFinal = finalDate;
+    }
+    if (this.selectedPrice?.durationType == '4') {
+      let anio: number = this.inscription.dateInscription.getFullYear();
+      let month: number =
+        this.selectedPrice?.duration! +
+        this.inscription.dateInscription.getMonth();
+      let day: number = this.inscription.dateInscription.getDate();
+      let finalDate = new Date(anio, month, day);
+      this.inscription.dateFinal = finalDate;
+    }
+    if (this.selectedPrice?.durationType == '5') {
+      let anio: number =
+        this.inscription.dateInscription.getFullYear() +
+        this.selectedPrice?.duration!;
+      let month: number = this.inscription.dateInscription.getMonth();
+      let day: number = this.inscription.dateInscription.getDate();
+      let finalDate = new Date(anio, month, day);
+      this.inscription.dateFinal = finalDate;
+    }
   }
 }
