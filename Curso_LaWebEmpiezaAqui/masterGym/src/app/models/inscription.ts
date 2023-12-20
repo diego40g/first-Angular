a3b0c5c2c4ce969bcb7fc1f9980e967dbf1a42cf
diego40g <diegopaznaula@hotmail.com>
2023-12-19 22:40:52 -0500

    Add validation method to Inscription model

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/models/inscription.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/models/inscription.ts
@@ -18,4 +18,50 @@ export class Inscription {
     this.iva = this.iva;
     this.total = this.total;
   }
+
+  validate(): any {
+    let response = {
+      isValid: false,
+      message: '',
+    };
+
+    if (this.client == null || this.client == undefined) {
+      response.isValid = false;
+      response.message = 'Select the client is required';
+      return response;
+    }
+    if (this.dateInscription == null || this.dateInscription == undefined) {
+      response.isValid = false;
+      response.message = 'Date inscription is required';
+      return response;
+    }
+    if (this.dateFinal == null || this.dateFinal == undefined) {
+      response.isValid = false;
+      response.message = 'Date final is required';
+      return response;
+    }
+    if (this.price == null || this.price == undefined) {
+      response.isValid = false;
+      response.message = 'Price is required';
+      return response;
+    }
+    if (this.subTotal <= 0 || this.subTotal == undefined) {
+      response.isValid = false;
+      response.message = 'Subtotal is required';
+      return response;
+    }
+    if (this.iva <= 0 || this.iva == undefined) {
+      response.isValid = false;
+      response.message = 'IVA is required';
+      return response;
+    }
+    if (this.total <= 0 || this.total == undefined) {
+      response.isValid = false;
+      response.message = 'Total is required';
+      return response;
+    }
+
+    response.isValid = true;
+    return response;
+  }
 }
