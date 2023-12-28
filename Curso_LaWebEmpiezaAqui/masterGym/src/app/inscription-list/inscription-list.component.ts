2f474f48c3c5961631519afcff37e595c580165c
diego40g <diegopaznaula@hotmail.com>
2023-12-27 23:10:21 -0500

    Refactor InscriptionListComponent to fetch and assign client data

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription-list/inscription-list.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription-list/inscription-list.component.ts
@@ -5,9 +5,29 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-inscription-list',
   templateUrl: './inscription-list.component.html',
-  styleUrls: ['./inscription-list.component.sass']
+  styleUrls: ['./inscription-list.component.sass'],
 })
 export class InscriptionListComponent {
   inscriptions: Inscription[] = [];
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) {}
+
+  ngOnInit(): void {
+    this.db
+      .collection('inscriptions')
+      .get()
+      .subscribe((result) => {
+        result.forEach((inscription) => {
+          let inscriptionObject = inscription.data() as any;
+          inscriptionObject.id = inscription.id;
+
+          this.db
+            .doc(inscription.data().client.path)
+            .get()
+            .subscribe((client) => {
+              console.log(client.data());
+              inscriptionObject.client = client.data();
+            });
+        });
+      });
+  }
 }
