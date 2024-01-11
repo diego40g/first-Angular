6eb085bbc13d63a29e8bfffb66dd57faddb0eeb2
diego40g <diegopaznaula@hotmail.com>
2024-01-10 23:11:46 -0500

    Add Timestamp type to Inscription model and update date handling in InscriptionListComponent

--- Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription-list/inscription-list.component.ts
+++ Curso_LaWebEmpiezaAqui/masterGym/src/app/inscription-list/inscription-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Inscription } from '../models/inscription';
+import { Inscription, Timestamp } from '../models/inscription';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
@@ -12,26 +12,42 @@ export class InscriptionListComponent {
   constructor(private db: AngularFirestore) {}
 
   ngOnInit(): void {
+    // this.inscriptions.length = 0;
     this.db
       .collection('inscriptions')
       .get()
       .subscribe((result) => {
         result.forEach((inscription) => {
-          let inscriptionGetting = inscription.data() as any;
+          let inscriptionGetting = inscription.data() as Inscription;
           inscriptionGetting.id = inscription.id;
 
-          let clientData = inscription.data().client;
+          let clientData = inscriptionGetting.client as any;
 
-          console.log(inscription.data());
-
-          /*this.db
-            .doc(clientPath!.clientPath.path)
+          this.db
+            .doc(clientData.path)
             .get()
             .subscribe((client) => {
-              inscriptionGetting.client = client.data();
+              inscriptionGetting.client = client.data() as any;
+              let dateInscription: Timestamp | Date | null =
+                inscriptionGetting.dateInscription;
+              let dateFinal: Timestamp | Date | null =
+                inscriptionGetting.dateFinal;
+
+              if (dateInscription && 'seconds' in dateInscription) {
+                let timestamp = dateInscription as unknown as Timestamp;
+                inscriptionGetting.dateInscription = new Date(
+                  timestamp.seconds * 1000
+                );
+              }
+              if (dateFinal && 'seconds' in dateFinal) {
+                let timestamp = dateFinal as unknown as Timestamp;
+                inscriptionGetting.dateFinal = new Date(
+                  timestamp.seconds * 1000
+                );
+              }
               this.inscriptions.push(inscriptionGetting);
-              console.log(client.data());
-            });*/
+              console.log(inscriptionGetting);
+            });
         });
       });
   }
